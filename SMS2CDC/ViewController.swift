63da5019d0c499778f7cdca567c7fcc57684f703
Andy <38127872+AndyLin77@users.noreply.github.com>
2021-05-22 16:57:48 +0800

    Initial Commit

---
+++ SMS2CDC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SMS2CDC
+//
+//  Created by Andy.Lin on 2021/5/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SMS2CDCTests/SMS2CDCTests.swift
@@ -0,0 +1,33 @@
+//
+//  SMS2CDCTests.swift
+//  SMS2CDCTests
+//
+//  Created by Andy.Lin on 2021/5/22.
+//
+
+import XCTest
+@testable import SMS2CDC
+
+class SMS2CDCTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
